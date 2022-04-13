469420084368026bec7c665efb80babed6d7d05e
ahmadfaf08 <afafgaming123@gmail.com>
2022-04-14 05:36:05 +0700

    Add project files.

---
+++ MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/MenuLogin.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MenuMaster_WinFormsApp
+{
+    public partial class MenuLogin : Form
+    {
+        public MenuLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection konek = new SqlConnection (@"Data Source=DESKTOP-55FDHPN\AHMADFADHIL;Initial Catalog=MenuMaster;Integrated Security=True");
+            SqlDataAdapter adr = new SqlDataAdapter("select count (*) from Login where NamaUser = '" + textBox1.Text + "' and PasswordUser = '" + textBox2.Text + "'", konek);
+            DataTable dt = new DataTable();
+
+            adr.Fill(dt);
+
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                MenuUtama panggil = new MenuUtama();
+                panggil.Show();
+            }
+            else
+            {
+                MessageBox.Show("username atau password salah", "Warning !!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+    }
+}
