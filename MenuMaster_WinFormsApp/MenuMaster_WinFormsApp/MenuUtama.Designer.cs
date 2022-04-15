16f83ae0bbe057487fb8f10684f9454bbdd730b3
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2022-04-15 22:54:36 +0700

    Membuat Tampilan Menu

--- MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/MenuUtama.Designer.cs
+++ MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/MenuUtama.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Profile = new System.Windows.Forms.Button();
+            this.Keranjang = new System.Windows.Forms.Button();
+            this.Pencarian = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Profile
+            // 
+            this.Profile.Location = new System.Drawing.Point(12, 23);
+            this.Profile.Name = "Profile";
+            this.Profile.Size = new System.Drawing.Size(94, 29);
+            this.Profile.TabIndex = 1;
+            this.Profile.Text = "Profile";
+            this.Profile.UseVisualStyleBackColor = true;
+            // 
+            // Keranjang
+            // 
+            this.Keranjang.Location = new System.Drawing.Point(651, 374);
+            this.Keranjang.Name = "Keranjang";
+            this.Keranjang.Size = new System.Drawing.Size(94, 29);
+            this.Keranjang.TabIndex = 2;
+            this.Keranjang.Text = "Keranjang";
+            this.Keranjang.UseVisualStyleBackColor = true;
+            this.Keranjang.Click += new System.EventHandler(this.Keranjang_Click);
+            // 
+            // Pencarian
+            // 
+            this.Pencarian.Location = new System.Drawing.Point(459, 23);
+            this.Pencarian.Name = "Pencarian";
+            this.Pencarian.Size = new System.Drawing.Size(94, 29);
+            this.Pencarian.TabIndex = 3;
+            this.Pencarian.Text = "Pencarian";
+            this.Pencarian.UseVisualStyleBackColor = true;
+            // 
+            // MenuUtama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pencarian);
+            this.Controls.Add(this.Keranjang);
+            this.Controls.Add(this.Profile);
+            this.Name = "MenuUtama";
             this.Text = "MenuUtama";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button Profile;
+        private Button Keranjang;
+        private Button Pencarian;
     }
 }
--- MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/MenuUtama.cs
+++ MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/MenuUtama.cs
@@ -16,5 +16,10 @@ namespace MenuMaster_WinFormsApp
         {
             InitializeComponent();
         }
+
+        private void Keranjang_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
