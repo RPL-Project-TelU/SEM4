7e8d3fc0fee7ec42f80bba39bcabb39e4d34bea8
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2022-04-16 00:32:32 +0700

    Membuat Fitur Profile

--- MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/Profile.Designer.cs
+++ MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/Profile.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MenuMaster_WinFormsApp
 {
-    partial class MenuUtama
+    partial class Profile
     {
         /// <summary>
         /// Required designer variable.
@@ -32,37 +32,39 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Nama = new System.Windows.Forms.Label();
             this.Username = new System.Windows.Forms.Label();
-            this.Handphone = new System.Windows.Forms.Label();
-            this.Email = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Handphone = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.Alamat = new System.Windows.Forms.Label();
-            this.Foto = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.Foto)).BeginInit();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Kembali
             // 
-            this.Kembali.Location = new System.Drawing.Point(640, 373);
+            this.Kembali.Location = new System.Drawing.Point(642, 354);
             this.Kembali.Name = "Kembali";
             this.Kembali.Size = new System.Drawing.Size(94, 29);
             this.Kembali.TabIndex = 0;
             this.Kembali.Text = "Kembali";
             this.Kembali.UseVisualStyleBackColor = true;
+            this.Kembali.Click += new System.EventHandler(this.Kembali_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 70);
+            this.textBox1.Location = new System.Drawing.Point(172, 50);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(125, 27);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Nama
             // 
             this.Nama.AutoSize = true;
-            this.Nama.Location = new System.Drawing.Point(49, 73);
+            this.Nama.Location = new System.Drawing.Point(47, 57);
             this.Nama.Name = "Nama";
             this.Nama.Size = new System.Drawing.Size(49, 20);
             this.Nama.TabIndex = 2;
@@ -72,95 +74,95 @@
             // Username
             // 
             this.Username.AutoSize = true;
-            this.Username.Location = new System.Drawing.Point(49, 126);
+            this.Username.Location = new System.Drawing.Point(47, 115);
             this.Username.Name = "Username";
             this.Username.Size = new System.Drawing.Size(75, 20);
-            this.Username.TabIndex = 3;
+            this.Username.TabIndex = 4;
             this.Username.Text = "Username";
             // 
-            // Handphone
+            // textBox2
             // 
-            this.Handphone.AutoSize = true;
-            this.Handphone.Location = new System.Drawing.Point(49, 177);
-            this.Handphone.Name = "Handphone";
-            this.Handphone.Size = new System.Drawing.Size(87, 20);
-            this.Handphone.TabIndex = 4;
-            this.Handphone.Text = "Handphone";
+            this.textBox2.Location = new System.Drawing.Point(172, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 3;
             // 
             // Email
             // 
             this.Email.AutoSize = true;
-            this.Email.Location = new System.Drawing.Point(49, 229);
+            this.Email.Location = new System.Drawing.Point(47, 172);
             this.Email.Name = "Email";
             this.Email.Size = new System.Drawing.Size(46, 20);
-            this.Email.TabIndex = 5;
+            this.Email.TabIndex = 6;
             this.Email.Text = "Email";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(179, 119);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(125, 27);
-            this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(179, 170);
+            this.textBox3.Location = new System.Drawing.Point(172, 172);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(125, 27);
-            this.textBox3.TabIndex = 8;
+            this.textBox3.TabIndex = 5;
+            // 
+            // Handphone
+            // 
+            this.Handphone.AutoSize = true;
+            this.Handphone.Location = new System.Drawing.Point(47, 235);
+            this.Handphone.Name = "Handphone";
+            this.Handphone.Size = new System.Drawing.Size(87, 20);
+            this.Handphone.TabIndex = 8;
+            this.Handphone.Text = "Handphone";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(179, 226);
+            this.textBox4.Location = new System.Drawing.Point(172, 235);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(125, 27);
-            this.textBox4.TabIndex = 9;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(179, 271);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(125, 27);
-            this.textBox5.TabIndex = 10;
+            this.textBox4.TabIndex = 7;
             // 
             // Alamat
             // 
             this.Alamat.AutoSize = true;
-            this.Alamat.Location = new System.Drawing.Point(49, 278);
+            this.Alamat.Location = new System.Drawing.Point(47, 288);
             this.Alamat.Name = "Alamat";
             this.Alamat.Size = new System.Drawing.Size(57, 20);
-            this.Alamat.TabIndex = 6;
+            this.Alamat.TabIndex = 10;
             this.Alamat.Text = "Alamat";
             // 
-            // Foto
+            // textBox5
             // 
-            this.Foto.Location = new System.Drawing.Point(609, 35);
-            this.Foto.Name = "Foto";
-            this.Foto.Size = new System.Drawing.Size(125, 62);
-            this.Foto.TabIndex = 11;
-            this.Foto.TabStop = false;
+            this.textBox5.Location = new System.Drawing.Point(172, 288);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 9;
             // 
-            // MenuUtama
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(611, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 62);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Profile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Foto);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Alamat);
-            this.Controls.Add(this.Email);
+            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.Handphone);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.Username);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.Nama);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Kembali);
-            this.Name = "MenuUtama";
-            this.Text = "MenuUtama";
-            ((System.ComponentModel.ISupportInitialize)(this.Foto)).EndInit();
+            this.Name = "Profile";
+            this.Text = "Profile";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,13 +174,13 @@
         private TextBox textBox1;
         private Label Nama;
         private Label Username;
-        private Label Handphone;
-        private Label Email;
         private TextBox textBox2;
+        private Label Email;
         private TextBox textBox3;
+        private Label Handphone;
         private TextBox textBox4;
-        private TextBox textBox5;
         private Label Alamat;
-        private PictureBox Foto;
+        private TextBox textBox5;
+        private PictureBox pictureBox1;
     }
 }
