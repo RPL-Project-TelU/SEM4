127a8dbd22ed2a3c5236a741fcc25ace627ae88b
dskresty <100820249+dskresty@users.noreply.github.com>
2022-04-15 22:44:16 +0700

    Registrasi

--- MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/MenuLogin.cs
+++ MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/MenuLogin.cs
@@ -39,8 +39,7 @@ namespace MenuMaster_WinFormsApp
 
 
         }
-<<<<<<< Updated upstream
-=======
+
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
@@ -68,6 +67,5 @@ namespace MenuMaster_WinFormsApp
             Registrasi panggil = new Registrasi();
             panggil.Show();
         }
->>>>>>> Stashed changes
     }
 }
