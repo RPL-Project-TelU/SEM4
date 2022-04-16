089e7d55d1d73c74a8b9971b45db5d1ba1ebefe9
Rizky Fajri <mansamussolini@gmail.com>
2022-04-16 11:42:28 +0700

    Config_File

---
+++ MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/JSON_Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuMaster_WinFormsApp
+{
+    public class JSON_Config
+    {
+        public string Lang { get; set; }
+    }
+}
---
+++ MenuMaster_WinFormsApp/MenuMaster_WinFormsApp/ReadWrite_JSON.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MenuMaster_WinFormsApp
+{
+    public class ReadWrite_JSON
+    {
+        public void ReadJson(JSON_Config data)
+        {
+            string file = JsonConvert.SerializeObject(data);
+            File.WriteAllText(@"\Config.json", file);
+        }
+        public JSON_Config WriteJson()
+        {
+            dynamic data = File.ReadAllText(@"\Config.json");
+            JSON_Config config = JsonConvert.DeserializeObject<JSON_Config>(data);
+            return config;
+        }
+    }
+}
