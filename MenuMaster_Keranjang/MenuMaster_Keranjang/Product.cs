c0dfb46a3bc320de74ddc2de210b98ba87909628
dskresty <100820249+dskresty@users.noreply.github.com>
2022-07-02 03:50:16 +0700

    keranjang + sonarqube

---
+++ MenuMaster_Keranjang/MenuMaster_Keranjang/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuMaster_Keranjang
+{
+    internal class Product
+    {
+        public string Deskripsi { get; set; }
+        public double Cost { get; set; }
+        public int NumberOrdered { get; set; }
+
+        public Product(string deskripsi, double cost)
+        {
+            this.Deskripsi = deskripsi;
+            this.Cost = cost;
+        }
+    }
+}
